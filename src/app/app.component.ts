f0b565dad57a7d75b99405098d99ce4f35a1d2d0
mwanginjamb <francnjamb@gmail.com>
2019-11-03 22:54:17 +0300

    First Branco Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './posts/posts.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'branco';
+  storedPosts: Post[] = [];
+
+  onPostAdded(post) {
+    this.storedPosts.push(post);
+  }
 }
