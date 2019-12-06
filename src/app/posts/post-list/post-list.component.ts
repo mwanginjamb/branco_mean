d88b8f649cd126726a318b49ca5c013867a6334c
mwanginjamb <francnjamb@gmail.com>
2019-12-06 11:52:12 +0300

    ian work

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from '../posts.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -8,17 +9,16 @@ import { Post } from '../posts.model';
 })
 export class PostListComponent implements OnInit {
 
-  /* posts = [
-    {title: 'I am  a victor', content: 'Victor work smart and Hard' },
-    {title: 'Business', content: 'Business is making money.' },
-    {title: 'Society', content: 'A liberal society is benefitial' }
-  ]; */
 
- @Input() posts: Post[] = [];
 
-  constructor() { }
+  posts: Post[] = [];
+
+  constructor(public postsService: PostsService) {
+
+   }
 
   ngOnInit() {
+
   }
 
 }
